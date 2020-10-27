43d8f0aea493978cc1b411887703793f713dcf03
Dillon <17171875+DevPowers@users.noreply.github.com>
2020-10-26 22:27:09 -0400

    Updated file structure

---
+++ build/precache-manifest.4e058f9567983fd8895f3c65168aee1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f4512032dc8aa950c0e8b862fe1b8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd20d8b8ec1677f82cbc",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "32315aab3d7c6cb0cc56",
+    "url": "/static/js/2.b441d618.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.b441d618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd20d8b8ec1677f82cbc",
+    "url": "/static/js/main.c3f18052.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
